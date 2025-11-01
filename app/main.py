afe7ca5f7730b8413803df554b53a77406459b29
abhinit1999 <krabhinit60@gmail.com>
2025-11-01 15:38:03 +0530

    done

--- app/main.py
+++ app/main.py
@@ -5,17 +5,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="AI-Browser")
 app.include_router(router)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000"],  # React dev server
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://youraibrowser.vercel.app/"],  # Or specify your frontend domain
+                   "https://youraibrowser.vercel.app"],  # Or specify your frontend domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
