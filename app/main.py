8031f6fcabbde988ce0c7119f3d6150a211ec5bd
abhinit1999 <krabhinit60@gmail.com>
2025-11-01 15:26:26 +0530

    added frontEnd url

--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ app.include_router(router)
 # )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Or specify your frontend domain
+    allow_origins=["http://localhost:3000",
+                   "https://youraibrowser.vercel.app/"],  # Or specify your frontend domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
