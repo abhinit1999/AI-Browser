d10632f1ae8decb4662fb980442f2de6aeac7fc5
abhinit1999 <krabhinit60@gmail.com>
2025-11-01 10:26:37 +0530

    config file setup done

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+    MODEL_NAME = "openai/gpt-oss-20b"
+
+settings = Settings()
